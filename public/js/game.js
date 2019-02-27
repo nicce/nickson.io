4192726c894cbb3e7935170904385b650619801d
Niclas Fredriksson <niclas.fredriksson@tretton37.com>
2019-02-27 21:25:51 +0100

    show player on canvas

--- public/js/game.js
+++ public/js/game.js
@@ -22,10 +22,29 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+    this.load.image('ship', 'assets/spaceShips_001.png');
+}
 
 function create() {
     this.socket = io();
+    this.socket.on('currentPlayers', (players) => {
+        Object.keys(players).forEach((id) => {
+            if(players[id].playerId === this.socket.id) {
+                addPlayer(this, players[id]);
+            }
+        })
+    })
 }
 
 function update() {}
+
+function addPlayer(self, playerInfo) {
+    self.ship = self.physics.add
+        .image(playerInfo.x, playerInfo.y, 'ship')
+        .setOrigin(0.5, 0.5)
+        .setDisplaySize(53, 40);
+    self.ship.setDrag(100);
+    self.ship.setAngularDrag(100);
+    self.ship.setMaxVelocity(200);
+}
--- server.js
+++ server.js
@@ -3,19 +3,43 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
+var players = {};
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('disconnect', function () {
+    addPlayer(socket);
+    socket.on('disconnect', () => {
+        removePlayer(socket);
         console.log('user disconnected');
     });
 });
 
-server.listen(8080, function () {
+server.listen(8080, () => {
     console.log(`Listening on ${server.address().port}`);
 });
+
+let addPlayer = (socket) => {
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+    };
+
+    //Send the players object to the new player
+    socket.emit('currentPlayers', players);
+    //update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+};
+
+let removePlayer = (socket) => {
+    delete players[socket.id];
+    io.emit('disconnect', socket.id)
+
+};
